3e8d35f67255854258f9c445b4d222fe37e824e2
Naman Patel <namanp2166@gmail.com>
2025-08-19 21:48:09 +0530

    added versel.json

--- frontend/src/pages/Gallery.jsx
+++ frontend/src/pages/Gallery.jsx
@@ -10,22 +10,22 @@ const galleryImages = [
   {
     id: 2,
     src: "https://plus.unsplash.com/premium_photo-1679917152396-4b18accacb9d?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8c29sYXIlMjBwYW5lbHxlbnwwfHwwfHx8MA%3D%3D",
-    alt: "Teamwork and Sustainable Planning",
+    alt: "Solar Energy Project",
   },
   {
     id: 3,
     src: "https://images.unsplash.com/photo-1716479852874-22742b84fef5?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8c3VzdGFpbmFibGUlMjBkZXZlbG9wbWVudHxlbnwwfHwwfHx8MA%3D%3D",
-    alt: "Architects Planning Eco-Friendly Buildings",
+    alt: "Economical Planning",
   },
   {
     id: 4,
     src: "https://images.unsplash.com/photo-1504384308090-c894fdcc538d",
-    alt: "Solar Energy Project",
+    alt: "Teamwork and Sustainable Planning",
   },
   {
     id: 5,
     src: "https://images.unsplash.com/photo-1451976426598-a7593bd6d0b2?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTl8fHN1c3RhaW5hYmxlJTIwZGV2ZWxvcG1lbnR8ZW58MHx8MHx8fDA%3D",
-    alt: "Sustainability Consultancy Meeting",
+    alt: "Architects Planning Eco-Friendly Buildings",
   },
   {
     id: 6,
